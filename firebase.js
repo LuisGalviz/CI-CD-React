74e6696873e14177f2dbc80d6a7caac011c69286
Luis Fernando Galviz <lgalviz@uninorte.edu.co>
2024-05-04 10:33:32 -0500

    feat: 🎸 first commit

---
+++ firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBHO-npZZ2O9E8Usj7OnSSDuK322n96THo",
+  authDomain: "especializacion-f9d16.firebaseapp.com",
+  projectId: "especializacion-f9d16",
+  storageBucket: "especializacion-f9d16.appspot.com",
+  messagingSenderId: "342773616220",
+  appId: "1:342773616220:web:171475be7798668c9ba099"
+};
+
+// Inicializa Firebase
+const app = initializeApp(firebaseConfig);
